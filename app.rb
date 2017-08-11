e66995a475133ff342a53463ddc181c2eaa267da
kostas delimbeis <kdelimbeis@comcast.net>
2017-08-11 16:37:33 -0400

    signup

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'bundler/setup'
 require 'sinatra/flash'
-
+require 'pry'
 require './models'
 
 
@@ -13,4 +13,29 @@ enable :sessions
 
 get '/' do
   erb :index
+end
+
+get '/signup' do
+  erb :signup
+end
+
+
+post '/login' do
+	user = User.find_by(username: params[:username])
+	if user && user.password == params[:password]
+		session[:user_id] = user.id
+		redirect '/'
+	else
+		redirect '/signup'
+	end
+end
+
+post '/logout' do
+	session[:user_id] = nil
+	redirect '/signup'
+end
+
+post '/new_user' do
+	User.create(username: params[:username], password: params[:password], city: params[:city], school: params[:school])
+	redirect "/"
 end
